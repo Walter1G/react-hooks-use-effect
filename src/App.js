0f5e51dac5fbf8466ca6834c7506c38e94f09456
Walter N <2760walter@gmail.com>
2023-10-29 17:40:57 +0300

    complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 import React, { useState, useEffect } from "react";
 
 function App() {
-  useEffect(() => {
-    console.log("useEffect called");
-  });
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+  const [images, setImages] = useState([])
+
+  // useEffect(() => {
+  //   console.log("useEffect called");
+  // });
 
   console.log("Component rendering");
 
-  return <button>Click Me</button>;
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random/3")
+      .then((r) => r.json())
+      .then((data) => {
+        setImages(data.message)
+      })
+  }, [])
+
+  return (
+    <>
+      <button onClick={() => setCount((count) => count + 1)}>
+        I've been Clicked {count} times
+      </button>
+      <input type="text"
+        placeholder="Type away..."
+        value={text}
+        onChange={(e) => setText(e.target.value)}>
+      </input>
+      <div>
+        {images.map(image => (
+          <img src={image} key={image}></img>
+        ))}
+      </div>
+    </>
+  )
 }
 
 export default App;
